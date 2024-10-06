f4066aa97442a150cda9541c9cce6b50d1df58bf
Valentin <valentin.geras@yandex.ru>
2024-10-06 10:27:35 +0300

    Старт проекта

---
+++ src/main/java/com/example/doll_collection/DollCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.doll_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doll_collection/DollCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doll_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
