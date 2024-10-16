0354fccdae3406deb6a674f34e30ecb4d04172b0
Valentin <valentin.geras@yandex.ru>
2024-10-16 21:26:33 +0300

    добавлена basic-аутентификация между сервисами

--- user-app/src/main/java/ru/collection/doll_collection/config/ClientBean.java
+++ user-app/src/main/java/ru/collection/doll_collection/config/ClientBean.java
@@ -2,6 +2,7 @@ package ru.collection.doll_collection.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import ru.collection.doll_collection.client.DollUserClientImpl;
 
@@ -10,8 +11,11 @@ public class ClientBean {
 
     @Bean
     public DollUserClientImpl dollUserClient() {
+        String serviceUserName = "catalog-service-user";
+        String serviceUserPassword = "Y2F0YWxvZy1zZXJ2aWNlLXVzZXI6cGFzc3dvcmQ=";
         return new DollUserClientImpl(RestClient.builder()
                 .baseUrl("http://localhost:8080")
+                .requestInterceptor(new BasicAuthenticationInterceptor(serviceUserName, serviceUserPassword))
                 .build());
     }
 
