c3158aefff29b7759a6ed9b101b434c7a3f979dc
Valentin <valentin.geras@yandex.ru>
2024-10-26 20:22:23 +0300

    добавили eureka

--- security-service/src/main/java/ru/collection/doll_collection/SecurityService.java
+++ security-service/src/main/java/ru/collection/doll_collection/SecurityService.java
@@ -2,8 +2,10 @@ package ru.collection.doll_collection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SecurityService {
 
     public static void main(String[] args) {
